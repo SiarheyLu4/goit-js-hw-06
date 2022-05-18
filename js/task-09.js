945f768929d7f6b124a288ac8b8dc3ccb132db37
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-18 16:48:36 +0300

    +9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = bodyEl.style.backgroundColor;
+
+});
+
+
+
+
+
+
+console.log(buttonEl)
+
+
+
+
+
+
+// const changeBtnColor=()=>{
+//   bodyEl.style.backgroundColor=getRandomHexColor();
+//   colorName.textContent=bodyColor.style.backgroundColor
+// }
+// btnColor.addEventListener("click",changeBtnColor)
+
+
+
