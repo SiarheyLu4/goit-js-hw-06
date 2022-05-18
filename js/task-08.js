fd419ee8677a550435bd1616a733bf839899a8e0
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-18 15:07:52 +0300

    +8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        return console.log(window.alert("Please fill in all the fields!"));
+    } else {
+
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+        const formData = {
+            email,
+            password,
+        };
+        console.log(formData);
+    }
+    event.currentTarget.reset()
+})
