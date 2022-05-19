6c3d5f2ad7ee68842b755bcfeee12afd8df93b37
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-05-19 17:10:46 +0300

    task 6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const textInput = document.querySelector("#validation-input");
 const lengthInput = document.querySelector("input[data-length]")
 
 textInput.addEventListener("blur", () => {
-    if (lengthInput.value.length >= Number(lengthInput.dataset.length)) {
+    if (lengthInput.value.length === Number(lengthInput.dataset.length)) {
         textInput.classList.remove("invalid")
         textInput.classList.add("valid")
     } else {textInput.classList.remove("valid")
